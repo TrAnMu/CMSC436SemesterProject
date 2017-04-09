27e9e92eef24383ea09c3f2216b93ab6a0cf43e1
J. Liu <junze0407@gmail.com>
2017-04-08 20:48:27 -0400

    Profile page is now displayed

--- app/src/main/java/groupproject/cmsc436/flow/ProfileActivity.java
+++ app/src/main/java/groupproject/cmsc436/flow/ProfileActivity.java
@@ -1,13 +1,10 @@
 package groupproject.cmsc436.flow;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class ProfileActivity extends AppCompatActivity {
-
+public class ProfileActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile);
+    protected Fragment createFragment() {
+        return ProfileFragment.newInstance();
     }
 }
--- app/src/main/java/groupproject/cmsc436/flow/ProfileFragment.java
+++ app/src/main/java/groupproject/cmsc436/flow/ProfileFragment.java
@@ -1,8 +1,37 @@
 package groupproject.cmsc436.flow;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by Junze on 4/8/2017.
  */
 
-public class ProfileFragment {
+public class ProfileFragment extends Fragment {
+    private TextView usernameTextView;
+    private TextView likesTextView;
+    private TextView placesTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        usernameTextView = (TextView)view.findViewById(R.id.profile_username);
+        likesTextView = (TextView)view.findViewById(R.id.profile_likes);
+        placesTextView = (TextView)view.findViewById(R.id.profile_places_travel);
+
+        return view;
+    }
+
+
+
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
 }
