3d75f6059898eb66d620f8f36c51692d9fd1ac86
J. Liu <junze0407@gmail.com>
2017-04-11 23:36:14 -0400

    Added listener when login status changed

--- app/src/main/java/groupproject/cmsc436/flow/LogInFragment.java
+++ app/src/main/java/groupproject/cmsc436/flow/LogInFragment.java
@@ -1,6 +1,8 @@
 package groupproject.cmsc436.flow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import groupproject.cmsc436.flow.Service.DatabaseService;
 
@@ -21,6 +26,42 @@ public class LogInFragment extends Fragment {
     private AutoCompleteTextView emailTextView;
     private EditText passwordTextView;
     private Button signInButton;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                } else {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), LogInActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
 
     @Nullable
     @Override
@@ -49,11 +90,6 @@ public class LogInFragment extends Fragment {
         });
         return view;
     }
-
-
-
-
-
 
     public static Fragment newInstance() {
         return new LogInFragment();
