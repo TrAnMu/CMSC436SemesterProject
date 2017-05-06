f565ec1f2c4074943fb450a50fb8cebb39a19174
TrAnMu <Travis@wireless-10-105-4-16.umd.edu>
2017-05-06 15:14:51 -0400

    Framework Up
    -Layout file finished (?)
    -Intents and such between fragment and activity are up.
    -App name change.

--- app/src/main/java/groupproject/cmsc436/flow/EventActivity.java
+++ app/src/main/java/groupproject/cmsc436/flow/EventActivity.java
@@ -1,8 +1,26 @@
 package groupproject.cmsc436.flow;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Travis on 5/6/17.
  */
 
 public class EventActivity extends SingleFragmentActivity {
+    private static final String INTENT_CREATED = "INTENT_CREATED";
+
+    protected Fragment createFragment() {
+        Intent intent = getIntent();
+
+        return EventFragment.newInstance(intent.getStringExtra(INTENT_CREATED));
+    }
+
+    public static Intent newIntent(Context context, String eventID) {
+        Intent newIntent = new Intent(context, EventActivity.class);
+        newIntent.putExtra(INTENT_CREATED, eventID);
+
+        return newIntent;
+    }
 }
--- app/src/main/java/groupproject/cmsc436/flow/EventFragment.java
+++ app/src/main/java/groupproject/cmsc436/flow/EventFragment.java
@@ -1,10 +1,53 @@
 package groupproject.cmsc436.flow;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import groupproject.cmsc436.flow.Service.DatabaseService;
 
 /**
  * Created by Travis on 5/6/17.
  */
 
-public class EventFragment extends Fragment {
+public class EventFragment extends Fragment implements OnMapReadyCallback {
+    private static final String BUNDLE_ASSEMBLED = "BUNDLE_ASSEMBLED";
+    DatabaseService service;
+    Event event;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        service = DatabaseService.getDBService(getActivity());
+        Bundle bundle = getArguments();
+        String eventId = bundle.getString(BUNDLE_ASSEMBLED);
+        event = service.getEvent(eventId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        //Do some stuff.
+
+        return view;
+    }
+
+    public static EventFragment newInstance(String eventId) {
+        Bundle bundle = new Bundle();
+        EventFragment fragment = new EventFragment();
+
+        bundle.putString(BUNDLE_ASSEMBLED, eventId);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
 }
