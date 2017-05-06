416090e00f3fd1682eda46f51507779e6776529f
TrAnMu <Travis@wireless-10-105-4-16.umd.edu>
2017-05-06 11:02:37 -0400

    Set up!

---
+++ app/src/main/java/groupproject/cmsc436/flow/EventActivity.java
@@ -0,0 +1,8 @@
+package groupproject.cmsc436.flow;
+
+/**
+ * Created by Travis on 5/6/17.
+ */
+
+public class EventActivity extends SingleFragmentActivity {
+}
---
+++ app/src/main/java/groupproject/cmsc436/flow/EventFragment.java
@@ -0,0 +1,10 @@
+package groupproject.cmsc436.flow;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Travis on 5/6/17.
+ */
+
+public class EventFragment extends Fragment {
+}
