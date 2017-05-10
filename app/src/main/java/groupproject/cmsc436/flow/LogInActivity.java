980b16ae8b30c08edc7c1ea0835b07acf16db9f9
J. Liu <junze0407@gmail.com>
2017-05-10 16:47:05 -0400

    Log in activity will not use a base activity for it

--- app/src/main/java/groupproject/cmsc436/flow/LogInActivity.java
+++ app/src/main/java/groupproject/cmsc436/flow/LogInActivity.java
@@ -1,14 +1,90 @@
 package groupproject.cmsc436.flow;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import groupproject.cmsc436.flow.Service.DatabaseService;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LogInActivity extends SingleFragmentActivity {
+public class LogInActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView emailTextView;
+    private EditText passwordTextView;
+    private Button signInButton;
+    private Button registerButton;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
-    protected Fragment createFragment() {
-        return LogInFragment.newInstance();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_log_in);
+        emailTextView = (AutoCompleteTextView) findViewById(R.id.email);
+        passwordTextView = (EditText) findViewById(R.id.password);
+        signInButton = (Button) findViewById(R.id.email_sign_in_button);
+        registerButton = (Button) findViewById(R.id.action_register_button);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), R.string.none_fields_blank, Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        DatabaseService.getDBService().signIn(email, password);
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), R.string.auth_failed, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
     }
 }
 
